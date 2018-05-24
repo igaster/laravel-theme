6cb6925ab4a4fed56ce93eafb8c00770af307c9c
Giannis Gasteratos <igasteratos@gmail.com>
2018-05-24 07:50:43 +0000

    Fix issue #93: @css('path/to/file.css') no longer working

--- src/themeServiceProvider.php
+++ src/themeServiceProvider.php
@@ -1,27 +1,28 @@
 <?php namespace Igaster\LaravelTheme;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Blade;
-use Igaster\LaravelTheme\Facades\Theme;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
 
-class themeServiceProvider extends ServiceProvider {
+class themeServiceProvider extends ServiceProvider
+{
 
-    public function register(){
+    public function register()
+    {
 
-		/*--------------------------------------------------------------------------
-		| Bind in IOC
-		|--------------------------------------------------------------------------*/
+        /*--------------------------------------------------------------------------
+        | Bind in IOC
+        |--------------------------------------------------------------------------*/
 
-		$this->app->singleton('igaster.themes', function(){
-			return new Themes();
-		});
+        $this->app->singleton('igaster.themes', function () {
+            return new Themes();
+        });
 
-		/*--------------------------------------------------------------------------
-		| Replace FileViewFinder
-		|--------------------------------------------------------------------------*/
+        /*--------------------------------------------------------------------------
+        | Replace FileViewFinder
+        |--------------------------------------------------------------------------*/
 
-        $this->app->singleton('view.finder', function($app) {
+        $this->app->singleton('view.finder', function ($app) {
             return new \Igaster\LaravelTheme\themeViewFinder(
                 $app['files'],
                 $app['config']['view.paths'],
@@ -29,39 +30,42 @@ class themeServiceProvider extends ServiceProvider {
             );
         });
 
-		/*--------------------------------------------------------------------------
-		| Register helpers.php functions
-		|--------------------------------------------------------------------------*/
+        /*--------------------------------------------------------------------------
+        | Register helpers.php functions
+        |--------------------------------------------------------------------------*/
 
         require_once 'Helpers/helpers.php';
 
-		/*--------------------------------------------------------------------------
-		| Initialize Themes
-		|--------------------------------------------------------------------------*/
+        /*--------------------------------------------------------------------------
+        | Initialize Themes
+        |--------------------------------------------------------------------------*/
 
-		$themes = $this->app->make('igaster.themes');
+        $themes = $this->app->make('igaster.themes');
         $themes->scanThemes();
 
-		/*--------------------------------------------------------------------------
-		| Activate default theme
-		|--------------------------------------------------------------------------*/
-		if (!$themes->current() && \Config::get('themes.default'))
-			$themes->set(\Config::get('themes.default'));
+        /*--------------------------------------------------------------------------
+        | Activate default theme
+        |--------------------------------------------------------------------------*/
+        if (!$themes->current() && \Config::get('themes.default')) {
+            $themes->set(\Config::get('themes.default'));
+        }
+
     }
 
-	public function boot(){
+    public function boot()
+    {
 
-		/*--------------------------------------------------------------------------
-		| Pulish configuration file
-		|--------------------------------------------------------------------------*/
+        /*--------------------------------------------------------------------------
+        | Pulish configuration file
+        |--------------------------------------------------------------------------*/
 
-		$this->publishes([
-			__DIR__.'/Config/themes.php' => config_path('themes.php'),
-		], 'laravel-theme');
+        $this->publishes([
+            __DIR__ . '/Config/themes.php' => config_path('themes.php'),
+        ], 'laravel-theme');
 
-		/*--------------------------------------------------------------------------
-		| Register Console Commands
-		|--------------------------------------------------------------------------*/
+        /*--------------------------------------------------------------------------
+        | Register Console Commands
+        |--------------------------------------------------------------------------*/
         if ($this->app->runningInConsole()) {
             $this->commands([
                 \Igaster\LaravelTheme\Commands\listThemes::class,
@@ -73,74 +77,73 @@ class themeServiceProvider extends ServiceProvider {
             ]);
         }
 
-		/*--------------------------------------------------------------------------
-		| Register custom Blade Directives
-		|--------------------------------------------------------------------------*/
+        /*--------------------------------------------------------------------------
+        | Register custom Blade Directives
+        |--------------------------------------------------------------------------*/
 
-		$this->registerBladeDirectives();
-	}
+        $this->registerBladeDirectives();
+    }
 
-	protected function registerBladeDirectives(){
-		/*--------------------------------------------------------------------------
-		| Extend Blade to support Orcherstra\Asset (Asset Managment)
-		|
-		| Syntax:
-		|
-		|   @css (filename, alias, depends-on-alias)
-		|   @js  (filename, alias, depends-on-alias)
-		|--------------------------------------------------------------------------*/
+    protected function registerBladeDirectives()
+    {
+        /*--------------------------------------------------------------------------
+        | Extend Blade to support Orcherstra\Asset (Asset Managment)
+        |
+        | Syntax:
+        |
+        |   @css (filename, alias, depends-on-alias)
+        |   @js  (filename, alias, depends-on-alias)
+        |--------------------------------------------------------------------------*/
 
-		Blade::extend(function($value)
-		{
-			return preg_replace_callback('/\@js\s*\(\s*([^),]*)(?:,\s*([^),]*))?(?:,\s*([^),]*))?\)/', function($match){
+        Blade::extend(function ($value) {
+            return preg_replace_callback('/\@js\s*\(\s*([^),]*)(?:,\s*([^),]*))?(?:,\s*([^),]*))?\)/', function ($match) {
 
-				$p1 = trim($match[1], " \t\n\r\0\x0B\"'");
-				$p2 = trim(empty($match[2]) ? $p1 : $match[2], " \t\n\r\0\x0B\"'");
-				$p3 = trim(empty($match[3]) ? '' : $match[3], " \t\n\r\0\x0B\"'");
+                $p1 = trim($match[1], " \t\n\r\0\x0B\"'");
+                $p2 = trim(empty($match[2]) ? $p1 : $match[2], " \t\n\r\0\x0B\"'");
+                $p3 = trim(empty($match[3]) ? '' : $match[3], " \t\n\r\0\x0B\"'");
 
-				if(empty($p3))
-					return "<?php Asset::script('$p2', theme_url('$p1'));?>";
-				else
-					return "<?php Asset::script('$p2', theme_url('$p1'), '$p3');?>";
+                if (empty($p3)) {
+                    return "<?php Asset::script('$p2', theme_url('$p1'));?>";
+                } else {
+                    return "<?php Asset::script('$p2', theme_url('$p1'), '$p3');?>";
+                }
 
-			},$value);
-		});
+            }, $value);
+        });
 
-		\Blade::extend(function ($value)
-		{
-			return preg_replace_callback('/\@jsIn\s*\(\s*([^),]*)(?:,\s*([^),]*))?(?:,\s*([^),]*))?(?:,\s*([^),]*))?\)/',
-				function ($match) {
+        Blade::extend(function ($value) {
+            return preg_replace_callback('/\@jsIn\s*\(\s*([^),]*)(?:,\s*([^),]*))?(?:,\s*([^),]*))?(?:,\s*([^),]*))?\)/',
+                function ($match) {
 
-					$p1 = trim($match[1], " \t\n\r\0\x0B\"'");
-					$p2 = trim($match[2], " \t\n\r\0\x0B\"'");
-					$p3 = trim(empty($match[3]) ? $p2 : $match[3], " \t\n\r\0\x0B\"'");
-					$p4 = trim(empty($match[4]) ? '' : $match[4], " \t\n\r\0\x0B\"'");
+                    $p1 = trim($match[1], " \t\n\r\0\x0B\"'");
+                    $p2 = trim($match[2], " \t\n\r\0\x0B\"'");
+                    $p3 = trim(empty($match[3]) ? $p2 : $match[3], " \t\n\r\0\x0B\"'");
+                    $p4 = trim(empty($match[4]) ? '' : $match[4], " \t\n\r\0\x0B\"'");
 
-					if (empty($p4)) {
-						return "<?php Asset::container('$p1')->script('$p3', theme_url('$p2'));?>";
-					} else {
-						return "<?php Asset::container('$p1')->script('$p3', theme_url('$p2'), '$p4');?>";
-					}
+                    if (empty($p4)) {
+                        return "<?php Asset::container('$p1')->script('$p3', theme_url('$p2'));?>";
+                    } else {
+                        return "<?php Asset::container('$p1')->script('$p3', theme_url('$p2'), '$p4');?>";
+                    }
 
-				}, $value);
-		});
+                }, $value);
+        });
 
+        Blade::extend(function ($value) {
+            return preg_replace_callback('/\@css\s*\(\s*([^),]*)(?:,\s*([^),]*))?(?:,\s*([^),]*))?\)/', function ($match) {
 
-		Blade::extend(function($value)
-		{
-			return preg_replace_callback('/\@css\s*\(\stheme_url([^),]*)(?:,\s*([^),]*))?(?:,\s*([^),]*))?\)/', function($match){
+                $p1 = trim($match[1], " \t\n\r\0\x0B\"'");
+                $p2 = trim(empty($match[2]) ? $p1 : $match[2], " \t\n\r\0\x0B\"'");
+                $p3 = trim(empty($match[3]) ? '' : $match[3], " \t\n\r\0\x0B\"'");
 
-				$p1 = trim($match[1], " \t\n\r\0\x0B\"'");
-				$p2 = trim(empty($match[2]) ? $p1 : $match[2], " \t\n\r\0\x0B\"'");
-				$p3 = trim(empty($match[3]) ? '' : $match[3], " \t\n\r\0\x0B\"'");
+                if (empty($p3)) {
+                    return "<?php Asset::style('$p2', theme_url('$p1'));?>";
+                } else {
+                    return "<?php Asset::style('$p2', theme_url('$p1'), '$p3');?>";
+                }
 
-				if(empty($p3))
-					return "<?php Asset::style('$p2', theme_url('$p1'));?>";
-				else
-					return "<?php Asset::style('$p2', theme_url('$p1'), '$p3');?>";
-
-			},$value);
-		});
-	}
+            }, $value);
+        });
+    }
 
 }
